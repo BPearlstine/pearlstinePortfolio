f138d8cdf305cb017b998f823af1ecaec1bc38f3
BPearlstine <bgpearlstine@my.waketech.edu>
2018-04-02 20:28:35 -0400

    began work on blogs app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def blogHome(request):
+    blogs = Blog.objects
+    return render(request,'blog/blogHome.html', {'blogs':blogs})
