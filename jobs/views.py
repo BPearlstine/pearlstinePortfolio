4f4c955feea5024a9cdca4f73263e5ed890a3fed
Ben Pearlstine <bgpearlstine@my.waketech.edu>
2018-04-03 15:50:32 -0400

    added latest  blog entry to home

--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from blog.models import Blog
 from .models import Job
 
 # Create your views here.
 def home(request):
+    blog = Blog.objects.latest('id')
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'jobs/home.html', {'jobs':jobs,'blog':blog})
