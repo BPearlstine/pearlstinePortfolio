d4eca7f2ce56c7bb31cbbe319ffb27447b98552e
Ben Pearlstine <bgpearlstine@my.waketech.edu>
2018-04-03 15:02:13 -0400

    added accounts authentication login/logout

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/login.html',{'error':'Username or Password is incorrect.'})
+    else:
+        return render(request,'accounts/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
--- pearlstinePortfolio/urls.py
+++ pearlstinePortfolio/urls.py
@@ -23,5 +23,6 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
