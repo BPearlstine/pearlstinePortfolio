7cb664c9722fbd585efcae0955586fadd15449ad
Ben Pearlstine <bgpearlstine@my.waketech.edu>
2018-04-04 14:52:26 -0400

    addCard works

--- cards/urls.py
+++ cards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cardHome, name='cardHome'),
     path('<int:card_id>/', views.cardDetail, name='cardDetail'),
+    path('addCard/', views.addCard, name='addCard'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from .models import Card
+
 # Create your views here.
 def cardHome(request):
     cards = Card.objects
@@ -8,3 +11,25 @@ def cardHome(request):
 def cardDetail(request,card_id):
     card = get_object_or_404(Card, pk=card_id)
     return render(request, 'cards/cardDetail.html',{'card':card})
+
+@login_required(login_url="/accounts/login")
+def addCard(request):
+    if request.POST['cardName'] and request.POST['cardSet'] and request.POST['cardArtist'] and request.POST['cardQuantity']:
+        foundCard = False
+        for card in Card.objects.all():
+            if card.name == request.POST['cardName'] and card.set == request.POST['cardSet']:
+                card.quantity += int(request.POST['cardQuantity'])
+                card.save()
+                foundCard = True
+                return redirect('cardDetail',card.id)
+        if foundCard != True:
+            card = Card()
+            card.name = request.POST['cardName']
+            card.set = request.POST['cardSet']
+            card.artist = request.POST['cardArtist']
+            card.quantity = request.POST['cardQuantity']
+            card.save()
+            return redirect('cardDetail',card.id)
+
+    else:
+        return render(request, 'cards/cardHome.html', {'error':'All values must be filled out'})
