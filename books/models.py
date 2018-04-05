8c5252bf17f13150d6f5e421b09c442e7d076635
BPearlstine <bgpearlstine@my.waketech.edu>
2018-04-05 00:31:29 -0400

    google books api for book covers

--- books/models.py
+++ books/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+import requests
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=400)
     author = models.CharField(max_length=300)
     genre = models.CharField(max_length=200)
     isbn = models.CharField(max_length=50)
+
+    def cover(self):
+            payload = {'q':self.isbn,}
+            r = requests.get('https://www.googleapis.com/books/v1/volumes', params=payload)
+            return r.json()['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']
 
     def __str__(self):
         return self.title
