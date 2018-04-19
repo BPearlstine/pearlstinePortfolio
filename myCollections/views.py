4679e9e300793de5cf3583b1dee1b8a62fa9d0ea
Ben Pearlstine <bpearlstine@Lenovo.com>
2018-04-19 10:36:02 -0400

    trying out datatables, added to collections home

--- myCollections/views.py
+++ myCollections/views.py
@@ -4,7 +4,7 @@ from books.models import Book
 from comics.models import Comic
 # Create your views here.
 def collections(request):
-    latest_cards = Card.objects.all().order_by('-id')[:3]
-    latest_books = Book.objects.all().order_by('-id')[:3]
-    latest_comics = Comic.objects.all().order_by('-id')[:3]
+    latest_cards = Card.objects.all()[:10]
+    latest_books = Book.objects.all()[:10]
+    latest_comics = Comic.objects.all()[:10]
     return render(request, 'myCollections/collections.html',{'latest_cards':latest_cards, 'latest_books':latest_books, 'latest_comics':latest_comics})
