8fde683ed44da7d72e177c1d70a4bc3aaa78475c
BPearlstine <bgpearlstine@my.waketech.edu>
2018-07-29 14:49:29 -0400

    switched cards to registered namespace

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = "cards"
+
 urlpatterns = [
     path('', views.cardHome, name='cardHome'),
     path('<int:card_id>/', views.cardDetail, name='cardDetail'),
