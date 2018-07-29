1fff1c22acde912550292497c03a9a11c3a6b4e2
BPearlstine <bgpearlstine@my.waketech.edu>
2018-07-29 14:45:22 -0400

    switched blog to registered namespace

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.blogHome, name='blogHome'),
     path('<int:blog_id>/', views.detail, name='detail'),
